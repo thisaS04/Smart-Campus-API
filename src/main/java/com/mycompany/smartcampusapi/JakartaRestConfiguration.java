678603fdf50cff929f3383add6a0a19e7a9b0eac
Thisarani Senevirathna <thisaranisenevi@gmail.com>
2026-04-15 14:43:52 +0530

    Initial Smart Campus API coursework

---
+++ src/main/java/com/mycompany/smartcampusapi/JakartaRestConfiguration.java
@@ -0,0 +1,9 @@
+package com.mycompany.smartcampusapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class JakartaRestConfiguration extends Application {
+    
+}
