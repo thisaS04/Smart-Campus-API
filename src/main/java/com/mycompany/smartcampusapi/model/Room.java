9edb240c50827779f397f795ed40cebfa3ab2adc
Thisarani Senevirathna <thisaranisenevi@gmail.com>
2026-04-16 18:49:12 +0530

    Created the Room.java class

--- src/main/java/com/mycompany/smartcampusapi/model/Room.java
+++ src/main/java/com/mycompany/smartcampusapi/model/Room.java
@@ -3,11 +3,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.smartcampusapi.model;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author user
  */
 public class Room {
+     private String id;
+    private String name;
+    private int capacity;
+    private List<String> sensorIds = new ArrayList<String>();
+
+    public Room() {
+    }
+
+    public Room(String id, String name, int capacity, List<String> sensorIds) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.sensorIds = sensorIds != null ? sensorIds : new ArrayList<String>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public List<String> getSensorIds() {
+        return sensorIds;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setSensorIds(List<String> sensorIds) {
+        this.sensorIds = sensorIds != null ? sensorIds : new ArrayList<String>();
+    }
     
 }
